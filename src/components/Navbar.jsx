453b71ff4fdd47503e8b65bf9e97baf38f20c9df
KomalSrivastava <132221203+KomalSrivastava@users.noreply.github.com>
2024-06-29 13:05:42 +0530

    https://divyatiwar.github.io/signup-signin/ Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,13 +18,17 @@ function Navbar() {
                 </div>
 
                 <div className="links flex gap-10 px-20">
-                    {["Write with us", "AI Chat-bot", "About Us", "Insights", "Contact"].map((item, index) => (
+                    {["Write with us", "AI Chat-bot", "About Us", "Login/Signup", "Contact"].map((item, index) => (
                         <a
                             key={index}
                             className={`text-lg capitalize font-light ${index === 4 && "ml-32"}`}
                             href={item === "Write with us" ? "https://prateek2384.github.io/Research_Nexus/" : "#"}
                             target={item === "Write with us" ? "_blank" : ""}
                             rel={item === "Write with us" ? "noopener noreferrer" : ""}
+                            className={`text-lg capitalize font-light ${index === 4 && "ml-32"}`}
+                            href={item === "Login/Signup" ? "https://divyatiwar.github.io/signup-signin/" : "#"}
+                            target={item === "Login/Signup" ? "_blank" : ""}
+                            rel={item === "Login/Signup" ? "noopener noreferrer" : ""}
                         >
                             {item}
                         </a>
