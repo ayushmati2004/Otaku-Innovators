fe03f6d1a38106e8071cd8dca100825ede131046
Komal <komalkashyap2107@gmail.com>
2024-07-27 00:02:09 +0530

    update

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,122 +1,158 @@
-import { easeIn, motion, useAnimation } from 'framer-motion';
+import { easeIn, motion, useAnimation } from "framer-motion";
 import { Power4 } from "gsap/all";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Featured() {
-    const cards = [useAnimation(), useAnimation()];
+  const cards = [useAnimation(), useAnimation()];
 
-    const handleHover = (index) => {
-        cards[index].start({ y: "0" })
-    }
+  const handleHover = (index) => {
+    cards[index].start({ y: "0" });
+  };
 
-    const handleHoverEnd = (index) => {
-        cards[index].start({ y: "100%" })
-    }
+  const handleHoverEnd = (index) => {
+    cards[index].start({ y: "100%" });
+  };
 
-    motion
-    return (
-        <div data-scroll="" data-scroll-section="" className="w-full py-20 ">
-            <div className="w-full px-20 border-b-[1px) border-zinc-700 pb-20">
-                <h1 className='text-7xl font-["Neue_Montereal"]'>
-                    Featured projects
-                </h1>
+  motion;
+  return (
+    <div data-scroll="" data-scroll-section="" className="w-full py-20 ">
+      <div className="w-full px-20 border-b-[1px) border-zinc-700 pb-20">
+        <h1 className='text-7xl font-["Neue_Montereal"]'>Featured projects</h1>
+      </div>
+      <div className="px-20">
+        <div className="cards w-full flex gap-10 mt-10">
+          <motion.div
+            onHoverStart={() => handleHover(0)}
+            onHoverEnd={() => handleHoverEnd(0)}
+            className="cardcontainer relative w-1/2 h-[75vh]"
+          >
+            <h1 className="absolute flex text-[#254E58] overflow-hidden right-0 translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl">
+              {"Pragmatism & Idealism".split("").map((item, index) => (
+                <motion.span
+                  initial={{ y: "100%" }}
+                  animate={cards[0]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    delay: index * 0.01,
+                  }}
+                  className="inline-block"
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
+
+            <div className="card w-full h-full rounded-xl overflow-hidden ">
+              <a href="https://www.geeksforgeeks.org/difference-between-pragmatism-and-idealism/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
+                <img
+                  className="w-full h-full bg-cover"
+                  src="https://cdn.sketchbubble.com/pub/media/catalog/product/optimized1/d/4/d4b7d60a72614d468ff9e7f527f64b5fe4f0b08da5304de2493fcd9d2ad71140/pragmatism-slide2.png"
+                  alt=""
+                />
+              </a>
             </div>
-            <div className="px-20">
-                <div className="cards w-full flex gap-10 mt-10">
-                    <motion.div
-                        onHoverStart={() => handleHover(0)}
-                        onHoverEnd={() => handleHoverEnd(0)}
-                        className="cardcontainer relative w-1/2 h-[75vh]">
-                        <h1 className='absolute flex text-[#254E58] overflow-hidden right-0 translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
-                            {"Pragmatism & Idealism".split("").map((item, index) => (
-                                <motion.span initial={{ y: "100%" }}
-                                    animate={cards[0]}
-                                    transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .01 }}
-                                    className="inline-block">
-                                    {item}
-                                </motion.span>
-                            ))}
-                        </h1>
+          </motion.div>
 
-                        <div className="card w-full h-full rounded-xl overflow-hidden "><a href="https://www.geeksforgeeks.org/difference-between-pragmatism-and-idealism/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
-                            <img className='w-full h-full bg-cover' src="https://cdn.sketchbubble.com/pub/media/catalog/product/optimized1/d/4/d4b7d60a72614d468ff9e7f527f64b5fe4f0b08da5304de2493fcd9d2ad71140/pragmatism-slide2.png" alt="" /></a>
+          <motion.div
+            onHoverStart={() => handleHover(1)}
+            onHoverEnd={() => handleHoverEnd(1)}
+            className="cardcontainer relative w-1/2 h-[75vh]"
+          >
+            <h1 className="absolute flex text-[#254E58] overflow-hidden right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl">
+              {"Disinfection & Sterilization".split("").map((item, index) => (
+                <motion.span
+                  initial={{ y: "100%" }}
+                  animate={cards[1]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    delay: index * 0.01,
+                  }}
+                  className="inline-block"
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
 
-                        </div>
-                    </motion.div>
-
-
-                    <motion.div
-                        onHoverStart={() => handleHover(1)}
-                        onHoverEnd={() => handleHoverEnd(1)}
-                        className="cardcontainer relative w-1/2 h-[75vh]">
-                        <h1 className='absolute flex text-[#254E58] overflow-hidden right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
-                            {"Disinfection & Sterilization".split("").map((item, index) => (
-                                <motion.span initial={{ y: "100%" }}
-                                    animate={cards[1]}
-                                    transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .01 }}
-                                    className="inline-block">
-                                    {item}
-                                </motion.span>
-                            ))}
-                        </h1>
-
-                        <div className="card w-full h-full rounded-xl overflow-hidden "><a href="https://www.geeksforgeeks.org/difference-between-disinfection-and-sterilization/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
-                            <img className='w-full h-full bg-cover' src="https://img.freepik.com/free-vector/worker-with-biohazard-suit-disinfecting-particles-illustration_24877-71226.jpg" alt="" /></a>
-
-                        </div>
-                    </motion.div>
-
-                </div>
-                <div className="cards w-full flex gap-10 mt-10">
-                    <motion.div
-                        onHoverStart={() => handleHover(0)}
-                        onHoverEnd={() => handleHoverEnd(0)}
-                        className="cardcontainer relative w-1/2 h-[75vh]">
-                        <h1 className='absolute flex text-[#FFFFFF] overflow-hidden right-0 translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
-                            {"Asymmetric Encryption".split("").map((item, index) => (
-                                <motion.span initial={{ y: "100%" }}
-                                    animate={cards[0]}
-                                    transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .01 }}
-                                    className="inline-block">
-                                    {item}
-                                </motion.span>
-                            ))}
-                        </h1>
-
-                        <div className="card w-full h-full rounded-xl overflow-hidden "><a href="https://www.geeksforgeeks.org/what-is-asymmetric-encryption/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
-                            <img className='w-full h-full bg-cover' src="https://cdn.prod.website-files.com/66325be5c91d02733ec6bfcc/667fdd1b432df1a9f0a094e5_Research_Nexus-removebg1.png" alt="" /></a>
-
-                        </div>
-                    </motion.div>
-
-
-                    <motion.div
-                        onHoverStart={() => handleHover(1)}
-                        onHoverEnd={() => handleHoverEnd(1)}
-                        className="cardcontainer relative w-1/2 h-[75vh]">
-                        <h1 className='absolute flex text-[#FFFFFF] overflow-hidden right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl'>
-                            {"IPv6".split("").map((item, index) => (
-                                <motion.span initial={{ y: "100%" }}
-                                    animate={cards[1]}
-                                    transition={{ ease: [0.22, 1, 0.36, 1], delay: index * .01 }}
-                                    className="inline-block">
-                                    {item}
-                                </motion.span>
-                            ))}
-                        </h1>
-
-                        <div className="card w-full h-full rounded-xl overflow-hidden "><a href="https://www.geeksforgeeks.org/advantages-of-ipv6/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
-                            <img className='w-full h-full bg-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnEGurKHDPMlD-9GwZ5OlsoduxYvzdNxjX14EVzJLXs80Tq8C7U_pEFh2Xyazqiv3cEv4&usqp=CAU" alt="" /></a>
-
-                        </div>
-                    </motion.div>
-                </div>
-
-
+            <div className="card w-full h-full rounded-xl overflow-hidden ">
+              <a href="https://www.geeksforgeeks.org/difference-between-disinfection-and-sterilization/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
+                <img
+                  className="w-full h-full bg-cover"
+                  src="https://img.freepik.com/free-vector/worker-with-biohazard-suit-disinfecting-particles-illustration_24877-71226.jpg"
+                  alt=""
+                />
+              </a>
             </div>
-        </div >
-    );
+          </motion.div>
+        </div>
+        <div className="cards w-full flex gap-10 mt-10">
+          <motion.div
+            onHoverStart={() => handleHover(0)}
+            onHoverEnd={() => handleHoverEnd(0)}
+            className="cardcontainer relative w-1/2 h-[75vh]"
+          >
+            <h1 className="absolute flex text-[#FFFFFF] overflow-hidden right-0 translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl">
+              {"Asymmetric Encryption".split("").map((item, index) => (
+                <motion.span
+                  initial={{ y: "100%" }}
+                  animate={cards[0]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    delay: index * 0.01,
+                  }}
+                  className="inline-block"
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
+
+            <div className="card w-full h-full rounded-xl overflow-hidden ">
+              <a href="https://www.geeksforgeeks.org/what-is-asymmetric-encryption/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
+                <img
+                  className="w-full h-full bg-cover"
+                  src="https://assets-global.website-files.com/62def148e4e23334e2e28ae1/62df01d48ccbc01d1f176292_rrss_02-1024x1024.png"
+                  alt=""
+                />
+              </a>
+            </div>
+          </motion.div>
+
+          <motion.div
+            onHoverStart={() => handleHover(1)}
+            onHoverEnd={() => handleHoverEnd(1)}
+            className="cardcontainer relative w-1/2 h-[75vh]"
+          >
+            <h1 className="absolute flex text-[#FFFFFF] overflow-hidden right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] leading-none tracking-tighter text-8xl">
+              {"IPv6".split("").map((item, index) => (
+                <motion.span
+                  initial={{ y: "100%" }}
+                  animate={cards[1]}
+                  transition={{
+                    ease: [0.22, 1, 0.36, 1],
+                    delay: index * 0.01,
+                  }}
+                  className="inline-block"
+                >
+                  {item}
+                </motion.span>
+              ))}
+            </h1>
+
+            <div className="card w-full h-full rounded-xl overflow-hidden ">
+              <a href="https://www.geeksforgeeks.org/advantages-of-ipv6/?utm_source=auth&utm_medium=saved&utm_campaign=articles">
+                <img
+                  className="w-full h-full bg-cover"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnEGurKHDPMlD-9GwZ5OlsoduxYvzdNxjX14EVzJLXs80Tq8C7U_pEFh2Xyazqiv3cEv4&usqp=CAU"
+                  alt=""
+                />
+              </a>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Featured;
-
--- src/components/LandinfPage.jsx
+++ src/components/LandinfPage.jsx
@@ -1,57 +1,64 @@
-import { motion } from 'framer-motion';
-import React from 'react';
+import { motion } from "framer-motion";
+import React from "react";
 import { FaArrowUpLong } from "react-icons/fa6";
 
-function LandinfPage() {
-    motion
-    return (
-        <div data-scroll-section="" data-scroll-speed="-0.8" className='w-full h-screen bg-grey-100 pt-1'>
-            <div className='textstructure mt-60 px-20'>
-                {["WE DELIVER", "INSIGHTFUL", "PUBLICATIONS"].map((item, index) => {
-                    return <div className="masker">
-                        <div className="w-fit flex items-end overflow-hidden">
-                            {index === 1 && (
-                                <motion.div initial={{ width: 0 }} animate={{ width: "9vw" }} transition={{ ease: [0.76, 0, 0.24, 1] }} className="mr-[1vw] w-[9vw] rounded-tl rounded-tr rounded-bl rounded-br h-[7.5vw] top-[1vw] relative bg-[#254E58]"></motion.div>
-                            )}
-                            < h1 className="pt-[0.5vw] uppercase text-[7.8vw] leading-[7vw] tracking-tighter font-['FF Basic Gothic'] font-medium">{item}</h1>
-                        </div>
-                    </div>
-                })}
-
-
-
-            </div >
-            <div className="border-t-[1px] border-zince-800 mt-20 flex justify-between items-center py-5 px-20">
-                {["For Organisations of all sizes", "From Discovery to innovation"].map((item, index) => <p className="text-md font-lighttracking-tight leading-none">{item}
-                </p>)}
-                <div className="start flex items-center gap-5">
-                    <a
-                        href="https://prateek2384.github.io/Research_Nexus/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="px-5 py-2 border-[1px] border-zinc-400 font-light text-md uppercase rounded-full"
-                    >
-                        START WRITING
-                    </a>
-                    <div className="w-10 h-10 flex items-center justify-center border-[1px] border-zinc-400 rounded-full">
-                        <span className="rotate-[45deg]">
-                            <FaArrowUpLong />
-                        </span>
-                    </div>
-                </div>
+function LandingPage() {
+  return (
+    <div
+      data-scroll-section=""
+      data-scroll-speed="-0.8"
+      className="w-full h-screen bg-grey-100 pt-1"
+    >
+      <div className="textstructure mt-60 px-20">
+        {["WE DELIVER", "INSIGHTFUL", "PUBLICATIONS"].map((item, index) => {
+          return (
+            <div key={index} className="masker">
+              <div className="w-fit flex items-end overflow-hidden">
+                {index === 1 && (
+                  <motion.div
+                    initial={{ width: 0 }}
+                    animate={{ width: "9vw" }}
+                    transition={{ ease: [0.76, 0, 0.24, 1] }}
+                    className="mr-[1vw] w-[9vw] rounded-tl rounded-tr rounded-bl rounded-br h-[7.5vw] top-[1vw] relative bg-[#254E58]"
+                  ></motion.div>
+                )}
+                <h1 className="pt-[0.5vw] uppercase text-[7.8vw] leading-[7vw] tracking-tighter font-['FF Basic Gothic'] font-medium">
+                  {item}
+                </h1>
+              </div>
             </div>
+          );
+        })}
+      </div>
+      <div className="border-t-[1px] border-zince-800 mt-20 flex justify-between items-center py-5 px-20">
+        {["For Organisations of all sizes", "From Discovery to innovation"].map(
+          (item, index) => (
+            <p
+              key={index}
+              className="text-md font-light tracking-tight leading-none hover:text-[#ADD8E6] hover:scale-105 transition-transform duration-300"
+            >
+              {item}
+            </p>
+          )
+        )}
+        <div className="start flex items-center gap-5">
+          <a
+            href="https://prateek2384.github.io/Research_Nexus/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="px-5 py-2 border-[1px] border-zinc-400 font-light text-md uppercase rounded-full hover:text-[#ADD8E6] hover:scale-105 transition-transform duration-300"
+          >
+            START WRITING
+          </a>
+          <div className="w-10 h-10 flex items-center justify-center border-[1px] border-zinc-400 rounded-full">
+            <span className="rotate-[45deg]">
+              <FaArrowUpLong />
+            </span>
+          </div>
         </div>
-
-
-    );
+      </div>
+    </div>
+  );
 }
 
-export default LandinfPage
-
-
-
-
-
-
-
-
+export default LandingPage;
